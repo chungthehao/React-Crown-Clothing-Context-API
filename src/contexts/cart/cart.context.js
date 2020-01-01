b8c10394a64f8fdbc17d5e542e831a4d09e67e35
Henry Chung <chungthehao@gmail.com>
2020-01-01 23:03:06 +0700

    Directory Context

--- src/contexts/cart/cart.context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-// Sẽ đc dynamic cập nhật thông qua local state ở component header rồi pass xuống qua provider component của chính nó.
-const CartContext = createContext({
-  hidden: true,
-  toggleCartHidden: () => {} // như 1 placeholder để lúc sau truyền 1 func vô
-});
-
-export default CartContext;
